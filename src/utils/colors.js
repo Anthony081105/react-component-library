303be9ccbceecf3eddff9d22d3399c84fa352aac
KevinYaozk <767008104@qq.com>
2022-04-13 17:44:17 +0800

    IMP: 新增scss样式支持

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -33,3 +33,38 @@ export const neutral = {
   500: "#4a4b53",
   600: "#000000"
 };
+
+
+// export const white = "#fff";
+// export const black = "#000";
+// export const gray = {
+//   100: "#f8f9fa",
+//   200: "#e9ecef",
+//   300: "#dee2e6",
+//   400: "#ced4da",
+//   500: "#adb5bd",
+//   600: "#6c757d",
+//   700: "#495057",
+//   800: "#343a40",
+//   900: "#212529",
+// };
+
+// export const blue = "#0d6efd";
+// export const indigo = "#6610f2";
+// export const purple = "#6f42c1";
+// export const pink = "#d63384";
+// export const red = "#dc3545";
+// export const Sorange = "#fd7e14";
+// export const yellow = "#fadb14";
+// export const green = "#52c41a";
+// export const Steal = "#20c997";
+// export const cyan = "#17a2b8";
+
+// export const primary = blue;
+// export const secondary = gray[600];
+// export const success = green;
+// export const info = cyan;
+// export const warning = yellow;
+// export const danger = red;
+// export const light = gray[100];
+// export const dark = gray[800];
