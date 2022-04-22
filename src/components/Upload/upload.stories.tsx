9862b35fb29907f1e4fefc841202b2877ff34edb
KevinYaozk <767008104@qq.com>
2022-04-22 14:59:59 +0800

    ADD: Upload组件完善生命周期

--- src/components/Upload/upload.stories.tsx
+++ src/components/Upload/upload.stories.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import Upload from './upload'
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import Upload from "./upload";
 
+const checkFileSize = (file: File) => {
+  if (Math.round(file.size / 1024) > 60) {
+    alert("file too big");
+    return false;
+  }
+};
 const defaultUpload = () => (
-  <Upload action='https://www.mocky.io/v2/5cc8019d300000980a055e76'>Default button</Upload>
-)
+  <Upload
+    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+    onChange={action("changed")}
+    beforeUpload={checkFileSize}
+  />
+);
 
-
-
-storiesOf('Upload Component', module)
-  .add('Upload', defaultUpload)
+storiesOf("Upload Component", module).add("Upload", defaultUpload);
--- src/components/Upload/upload.tsx
+++ src/components/Upload/upload.tsx
@@ -4,13 +4,15 @@ import Button from "../Button";
 
 export interface UploadProps {
   action: string;
+  beforeUpload?: (file: File) => boolean | Promise<File>;
   onProgress?: (percentage: number, file: File) => void;
   onSuccess?: (data: any, file: File) => void;
   onError?: (err: any, file: File) => void;
+	onChange?: (file:File)=>void;
 }
 
 const Upload: FC<UploadProps> = (props) => {
-  const { action, onProgress, onSuccess, onError } = props;
+  const { action, onProgress, onSuccess, onError, beforeUpload, onChange } = props;
   const fileInput = useRef<HTMLInputElement>(null);
   const handleClick = () => {
     if (fileInput.current) {
@@ -30,37 +32,59 @@ const Upload: FC<UploadProps> = (props) => {
   const uploadFiles = (files: FileList) => {
     let postFiles = Array.from(files);
     postFiles.forEach((file) => {
-      const formData = new FormData();
-      formData.append(file.name, file);
-      axios.post(action, formData, {
+      if (!beforeUpload) {
+        post(file);
+      } else {
+        const result = beforeUpload(file);
+        if (result && result instanceof Promise) {
+          result.then((processedFile) => {
+            post(processedFile);
+          });
+        } else if (result !== false) {
+          post(file);
+        }
+      }
+      post(file);
+    });
+  };
+  // 网络交互相关操作
+  const post = (file: File) => {
+    const formData = new FormData();
+    formData.append(file.name, file);
+    axios
+      .post(action, formData, {
         headers: {
           // 传输文件更快的类型
           "Content-Type": "multipart/form-data",
         },
-				// 利用axios自带的API来计算当前progress
+        // 利用axios自带的API来计算当前progress
         onUploadProgress: (e) => {
-					let percentage = Math.round((e.loaded*100)/e.total) || 0;
-					if(percentage<100){
-						// 进度回调
-						if(onProgress){
-							onProgress(percentage,file);
-						}
-					}
-				},
-      }).then(res =>{
-				console.log('result',res);
-				if(onSuccess){
-					// 成功回调
-					onSuccess(res.data,file)
+          let percentage = Math.round((e.loaded * 100) / e.total) || 0;
+          if (percentage < 100) {
+            // 进度回调
+            if (onProgress) {
+              onProgress(percentage, file);
+            }
+          }
+        },
+      })
+      .then((res) => {
+        console.log("result", res);
+        if (onSuccess) {
+          // 成功回调
+          onSuccess(res.data, file);
+        }
+				if(onChange) {
+					onChange(file);
 				}
-			}).catch(err=>{
-				console.log('error',err);
-				if(onError){
-					// 错误回调
-					onError(err,file);
-				}
-			});
-    });
+      })
+      .catch((err) => {
+        console.log("error", err);
+        if (onError) {
+          // 错误回调
+          onError(err, file);
+        }
+      });
   };
   return (
     <div className="cherry-upload-component">
